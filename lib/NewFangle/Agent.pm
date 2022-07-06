1e8b90da438737500730e4549fec0d227f1cb901
José Joaquín Atria <jjatria@gmail.com>
2022-07-06 09:08:27 +0100

    Bump version

--- lib/NewFangle/Agent.pm
+++ lib/NewFangle/Agent.pm
@@ -46,7 +46,7 @@ my (
 our $TX;    # The current NewFangle transaction
 our $Trace; # Should this be traced
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 
 my $parse_includes = sub {
     my ( $type, $config ) = @_;
--- lib/NewFangle/Agent/Config.pm
+++ lib/NewFangle/Agent/Config.pm
@@ -14,7 +14,7 @@ use Carp 'croak';
 
 use namespace::clean;
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 
 my ( $config );
 
